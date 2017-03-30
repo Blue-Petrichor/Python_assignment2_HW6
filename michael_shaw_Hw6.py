a5697ce7a9b44633dbc6c6f6a0d6c1d5c2868fcc
Michael Shaw <Michaelshaw3@mail.weber.edu>
2017-03-30 13:00:49 -0600

    A revised edition of the hw6.py

--- michael_shaw_Hw6.py
+++ michael_shaw_Hw6.py
@@ -12,11 +12,53 @@ def help_function():
         To run file line should be typed as above
     """
 
+def urlGetFileFull():
+
+    """
+    Get the file from a url (web) location
+    """
+    with urlopen("http://icarus.cs.weber.edu/~hvalle/cs3030/data/error.log.full") as error:
+
+        error_file  = []                     # set an empty array
+        urls_d = {}                          # instanciate the dictionary
+
+        # Compile the regex and put it into the variable regex for user later
+        regex = re.compile(".*/.*")
+        for logs in error:
+            error_file.append(logs.decode("utf-8"))
+
+        for line in error_file:
+            line_words = line.split()
+            for words in line_words:
+                var = regex.match(words)
+                if var:
+                    if var.group() in urls_d:
+                        urls_d[var.group()] += 1
+                    else:
+                        urls_d[var.group()] = 1
+                    break
+        print ("*** Top 25 page errors ***")
+        sorted_urls_d = sorted(urls_d, key=urls_d.get, reverse=True)
+
+        # Create a count to list only the first 25 from the count list
+        count = 0
+        for i in sorted_urls_d:
+            count +=1
+            # break when count of 25 has been reached
+            if (count > 25):
+                break
+            else:
+                # Print dictionary count and position
+                print ("Count: ", urls_d[i], " Page: ", i)
+
+
+
 def urlGetFile():
     """
     Get the file from a url (web) location
     """
     with urlopen("http://icarus.cs.weber.edu/~hvalle/cs3030/data/error.log.test") as error:
+
         error_file  = []                     # set an empty array
         urls_d = {}                          # instanciate the dictionary
 
@@ -54,7 +96,7 @@ def main():
 	Main function
     """
     # call url get function that prints the file parsed into the array
-    urlGetFile()    
+    urlGetFileFull()    
     
 
     pass
@@ -63,18 +105,21 @@ if __name__ == "__main__":
     #call main fuction
 
     parser = argparse.ArgumentParser()
+    parser.add_argument("RunFileFull", help="Help")
+    args = parser.parse_args()
+
+    if args.RunFileFull == "RunFileFull":
+        main()
+
+    parser = argparse.ArgumentParser()
     parser.add_argument("RunFile", help="Help")
     args = parser.parse_args()
 
     if args.RunFile == "RunFile":
-        main()
-    elif args.RunFile != "RunFile":
-        print ("Usage: michael_shaw_Hw6.py FileName")
+        urlGetFile()
+    
+    if args.RunFileFull == "" or args.RunFile == "":
+        print ("Usage: michael_shaw_Hw6.py FileName or FileNameFull")
         print ("File should be typed Exactly as above!")
-        print ("")
-    elif args.RunFile == "":
-        print ("Usage: michael_shaw_Hw6.py FileName")
-        print ("File should be typed Exactly as above!")
-
     exit(0)
 
